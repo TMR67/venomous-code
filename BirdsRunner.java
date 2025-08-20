9bb2ab0d2b3e7d7a3a17ecc55e1a1bff5ec6a39c
Thejas6767 <thejasmr.xworkz@gmail.com>
2025-08-20 18:34:35 +0530

    java

---
+++ BirdsRunner.java
@@ -0,0 +1,40 @@
+class BirdsRunner{
+public static void main (String [] args){
+double totalBird = Birds.noOfBirds(0);
+  System.out.println("Total number of Birds="+totalBird+"lakhs");
+  
+  
+double totalBirds1 = Birds.noOfBirds(1);
+	System.out.println("Total number of Birds in Banglore="+totalBirds1+"lakhs");
+	
+double totalBirds2 = Birds.noOfBirds(2);
+  System.out.println("Total number of Birds in Manglore="+totalBirds2+"lakhs");
+  
+ double totalBirds3 = Birds.noOfBirds(3);
+  System.out.println("Total number of Birds in Shivamogga="+totalBirds3+"lakhs");
+  
+  double totalBirds4 = Birds.noOfBirds(4);
+  System.out.println("Total number of Birds in Kodagu="+totalBirds4+"lakhs");
+  double totalBirds5 = Birds.noOfBirds(5);
+  System.out.println("Total number of Birds in Thirthahalli="+totalBirds5+"lakhs");
+  
+  double totalBirds6 = Birds.noOfBirds(6);
+  System.out.println("Total number of Birds in Mandya="+totalBirds6+"lakhs");
+  
+  double totalBirds7 = Birds.noOfBirds(7);
+  System.out.println("Total number of Birds in Durga="+totalBirds7+"lakhs");
+  
+  double totalBirds8 = Birds.noOfBirds(8);
+  System.out.println("Total number of Birds in Kalburgi="+totalBirds8+"lakhs");
+  
+  double totalBirds9 = Birds.noOfBirds(9);
+  System.out.println("Total number of Birds in Tumkuru="+totalBirds9+"lakhs");
+  
+  double totalBirds10 = Birds.noOfBirds(10);
+  System.out.println("Total number of Birds in Ballari="+totalBirds10+"lakhs");
+  
+  double totalBirds11 = Birds.noOfBirds(11);
+  System.out.println("Total number of Birds in Hassan="+totalBirds11+"lakhs");
+
+}
+}
---
+++ StateRunner.java
@@ -0,0 +1,58 @@
+class StateRunner{
+public static void main(String [] args){
+double peoples1 = State.noOfPeoples(1);
+System.out.println("Total peoples in Karnataka="+peoples1);
+
+double peoples2 = State.noOfPeoples(2);
+System.out.println("Total peoples in Aarunachal Pradesh="+peoples2);
+
+double peoples3 = State.noOfPeoples(3);
+System.out.println("Total peoples Maharashtra="+peoples3);
+
+double peoples4 = State.noOfPeoples(4);
+System.out.println("Total peoples Goa="+peoples4);
+
+double peoples5 = State.noOfPeoples(5);
+System.out.println("Total peoples Himachal="+peoples5);
+
+double peoples6 = State.noOfPeoples(6);
+System.out.println("Total peoples Haryana="+peoples6);
+
+double peoples7 = State.noOfPeoples(7);
+System.out.println("Total peoples Tamil nadu="+peoples7);
+
+double peoples8 = State.noOfPeoples(8);
+System.out.println("Total peoples Jammu="+peoples8);
+
+double peoples9 = State.noOfPeoples(9);
+System.out.println("Total peoples Punjab="+peoples9);
+
+double peoples10 = State.noOfPeoples(10);
+System.out.println("Total peoples Uttar Pradesh="+peoples10);
+
+double peoples11 = State.noOfPeoples(11);
+System.out.println("Total peoples Assam="+peoples11);
+
+double peoples12 = State.noOfPeoples(12);
+System.out.println("Total peoples Telangana="+peoples12);
+
+double peoples13 = State.noOfPeoples(13);
+System.out.println("Total peoples Nagaland="+peoples13);
+
+double peoples14 = State.noOfPeoples(14);
+System.out.println("Total peoples Mizoram="+peoples14);
+
+double peoples15 = State.noOfPeoples(15);
+System.out.println("Total peoples Meghalaya="+peoples15);
+
+double peoples16 = State.noOfPeoples(16);
+System.out.println("Total peoples Gujarat="+peoples16);
+
+double peoples17 = State.noOfPeoples(17);
+System.out.println("Total peoples Manipur="+peoples17);
+
+double peoples18 = State.noOfPeoples(18);
+System.out.println("Total peoples ="+peoples18);
+}
+
+}
