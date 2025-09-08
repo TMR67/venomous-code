90f67919b504e979d4bbe29ca10dc06a366471cc
Thejas6767 <thejasmr.xworkz@gmail.com>
2025-09-08 15:18:53 +0530

    datatypes

---
+++ custamised operator/Apartment.java
@@ -0,0 +1,13 @@
+class Apartment{
+public Apartment(){
+System.out.println("this is Apartment where I stay");
+}
+public static void roomNos(){
+	System.out.println("Building name is Sky Toucher");
+
+}
+public static void Rooms(){
+	System.out.println("total rooms = 34334");
+
+}
+}
---
+++ custamised operator/ApartmentRunner.java
@@ -0,0 +1,9 @@
+class ApartmentRunner{
+	public static void main(String[] args){
+	Mall mall = new Mall();
+	mall.TotalFloors();
+
+System.out.println("this is Apartment Runner");
+
+}
+}
