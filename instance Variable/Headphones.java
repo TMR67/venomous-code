1bee4b97e1d7ab5f4ecba48b124254bdf2c78d07
Thejas6767 <thejasmr.xworkz@gmail.com>
2025-09-09 13:11:00 +0530

    re-initialization

---
+++ instance Variable/Headphones.java
@@ -0,0 +1,37 @@
+class Headphones {
+    public int batteryLife = 30;            
+    public String brand = "Sony";
+    public char soundQuality = 'A';         
+    public boolean wireless = true;
+    public float weight = 0.25f;            
+    public double price = 5999.99;
+    public long serialNumber = 123456789012L;
+
+    public void displayHeadphones() {
+        System.out.println("Battery Life: " + batteryLife + " hours");
+        System.out.println("Brand: " + brand);
+        System.out.println("Sound Quality Grade: " + soundQuality);
+        System.out.println("Wireless: " + wireless);
+        System.out.println("Weight: " + weight + " kg");
+        System.out.println("Price: ₹" + price);
+        System.out.println("Serial Number: " + serialNumber);
+		
+		
+    int batteryLife = 123;            
+    String brand = "Boat";
+    char soundQuality = 'B';         
+    boolean wireless = false;
+    float weight = 1.23f;            
+    double price = 999.99;
+    long serialNumber = 654674678876L;
+	
+	
+	System.out.println("Battery Life: " + batteryLife + " hours");
+        System.out.println("Brand: " + brand);
+        System.out.println("Sound Quality Grade: " + soundQuality);
+        System.out.println("Wireless: " + wireless);
+        System.out.println("Weight: " + weight + " kg");
+        System.out.println("Price: ₹" + price);
+        System.out.println("Serial Number: " + serialNumber);
+    }
+}
---
+++ instance Variable/HeadphonesRunner.java
@@ -0,0 +1,6 @@
+class HeadphonesRunner {
+    public static void main(String[] args) {
+        Headphones sony = new Headphones();
+        sony.displayHeadphones();
+    }
+}
