9ba74b8ab1e8e9a45eb9a8edcc37f04493b5de89
Thejas6767 <thejasmr.xworkz@gmail.com>
2025-09-16 16:30:14 +0530

    using for loop

--- Diff class accesing/Restaurant.java
+++ Diff class accesing/Restaurant.java
@@ -6,18 +6,3 @@ class Restaurant {
     }
 }
 
-class RestaurantRunner {
-    public static void main(String[] args) {
-        Restaurant restaurant = new Restaurant();
-
-        RestaurantMenu m1 = new RestaurantMenu("Pizza", 150);
-        RestaurantMenu m2 = new RestaurantMenu("Burger", 80);
-        RestaurantMenu m3 = new RestaurantMenu("Sandwich", 60);
-        RestaurantMenu m4 = new RestaurantMenu("Fries", 50);
-        RestaurantMenu m5 = new RestaurantMenu("Pasta", 120);
-
-        RestaurantMenu[] menu = {m1, m2, m3, m4, m5};
-
-        restaurant.restaurantDisplay(menu);
-    }
-}
