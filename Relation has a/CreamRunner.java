a599f020ce4b0ad44390a8606842b4379d06a255
Thejas6767 <thejasmr.xworkz@gmail.com>
2025-09-17 11:38:19 +0530

    has a relation

--- Relation has a/CreamRunner.java
+++
@@ -1,8 +0,0 @@
-class CreamRunner{
-public static void main(String[] args){
-Ice ice=new Ice("Vanila Flavour");	
-Cream cream=new Cream(ice);	
-	
-cream.displayCream();	
-}
-}
--- Relation has a/PenRunner.java
+++
@@ -1,8 +0,0 @@
-class PenRunner {
-    public static void main(String[] args) {
-        Ink ink = new Ink("Blue");
-        Pen pen = new Pen(ink);
-
-        pen.displayPen();
-    }
-}
