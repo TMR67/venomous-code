545c4ad154c85e850ed5eb36dd1d43d2737e9aa5
Thejas6767 <thejasmr.xworkz@gmail.com>
2025-09-23 16:51:33 +0530

    College

---
+++ Inheritence/CollegeInfo.java
@@ -0,0 +1,35 @@
+class CollegeInfo {
+    public static void name() {
+        System.out.println("Sreedevi Institute");
+    }
+    public static void location() {
+        System.out.println("Mangaluru");
+    }
+    public static void foundedYear() {
+        System.out.println("1880");
+    }
+    public static void numberOfStudents() {
+        System.out.println("25000");
+    }
+    public static void acceptanceRate() {
+        System.out.println("55%");
+    }
+    public static void numberOfMajors() {
+        System.out.println("100+");
+    }
+    public static void hasSportsTeam() {
+        System.out.println("Yes");
+    }
+    public static void hasCampusHairing() {
+        System.out.println("Yes");
+    }
+    public static void isPublic() {
+        System.out.println("true");
+    }
+    public static void mainStudent() {
+        System.out.println("Sanjay");
+    }
+}
+
+
+
---
+++ Inheritence/CollegeInfoRunner.java
@@ -0,0 +1,15 @@
+class CollegeInfoRunner {
+    public static void main(String[] args) {
+        University myUniversity = new University();
+        myUniversity.name();
+        myUniversity.location();
+        myUniversity.foundedYear();
+        myUniversity.numberOfStudents();
+        myUniversity.acceptanceRate();
+        myUniversity.numberOfMajors();
+        myUniversity.hasSportsTeam();
+        myUniversity.hasCampusHairing();
+        myUniversity.isPublic();
+        myUniversity.mainStudent();
+    }
+}
