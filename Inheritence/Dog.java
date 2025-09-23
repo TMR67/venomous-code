7cebccf095f46bafe942f018003d3ff3100048a3
Thejas6767 <thejasmr.xworkz@gmail.com>
2025-09-23 17:28:40 +0530

    Dog

---
+++ Inheritence/Dog.java
@@ -0,0 +1,35 @@
+class Dog {
+	public static void name(){
+		System.out.println("Buddy");
+	}
+	public static void breed(){
+		System.out.println("Golden Retriever");
+	}
+	public static void age(){
+		System.out.println("5");
+	}
+	public static void color(){
+		System.out.println("Golden");
+	}
+	public static void size(){
+		System.out.println("Large");
+	}
+	public static void temperament(){
+		System.out.println("Friendly");
+	}
+	public static void isTrained(){
+		System.out.println("true");
+	}
+	public static void weight(){
+		System.out.println("30 kg");
+	}
+	public static void favoriteToy(){
+		System.out.println("Ball");
+	}
+	public static void sound(){
+		System.out.println("Woof");
+	}
+}
+
+
+
---
+++ Inheritence/DogRunner.java
@@ -0,0 +1,15 @@
+class DogRunner{
+	public static void main(String [] args){
+		Poodle myDog = new Poodle();
+		myDog.name();
+		myDog.breed();
+		myDog.age();
+		myDog.color();
+		myDog.size();
+		myDog.temperament();
+		myDog.isTrained();
+		myDog.weight();
+		myDog.favoriteToy();
+		myDog.sound();
+	}
+}
